b66e922d49697881a0ba120428a9ffb18e89e2e3
V-U-Simon <V-U-Simon@yandex.ru>
2023-04-26 17:12:17 +0300

    ref templator

--- lesson_01/mvc_framework/templator.py
+++ lesson_01/mvc_framework/templator.py
@@ -1,24 +1,31 @@
 from jinja2 import Template
 from pathlib import Path
 
+TEMPLATE_FOLDER = 'templates'
 
-def render_from_line(template, context: dict):
-    template = Template(template)
+
+def render_from_line(template_line: str, context: dict):
+    """ Сформировать шаблон из текста """
+    template = Template(template_line)
     return template.render(**context)
 
 
-def render(template, context: dict):
-    with open(template, 'r') as f:
+def render(template_file, context: dict):
+    """ Сформировать шаблон из файла """
+
+    template_file = Path.cwd() / TEMPLATE_FOLDER / template_file
+
+    with open(template_file, 'r') as f:
         template = Template(f.read())
         return template.render(**context)
 
 
-if __name__ == '__main__':
-    # some test
+# if __name__ == '__main__':
+#     # some test
 
-    res = render_from_line('Hello {{ name }}', context={'name': 'John Doe'})
-    print(res)
+#     res = render_from_line('Hello {{ name }}', context={'name': 'John Doe'})
+#     print(res)
 
-    template_file = Path.cwd() / 'templates' / 'stub.html'
-    res = render(template_file, context={'name': 'John Doe'})
-    print(res)
+#     template_file = Path.cwd() / 'templates' / 'stub.html'
+#     res = render(template_file, context={'name': 'John Doe'})
+#     print(res)
